27d60ea07ac2001fe313186e8c6979a8cc6320c0
Josmadelmo Davi <josmadelmodavi@gmail.com>
2020-06-07 11:35:08 -0300

    Implementando service subtrair

--- api-gateway/index.php
+++ api-gateway/index.php
@@ -47,5 +47,35 @@ $app->group("/somar", function () use ($app) {
     });
 });
 
+/* SUBTRAIR */
+$app->group("/subtrair", function () use ($app) {
+
+    $app->post("[/{params:.*}]", function (Request $request, Response $response, $args) {
+
+        if (!empty($args["params"]) && $args["params"] == "resultado") {
+            $novaRota = "operacao";
+            $url = "http://localhost/api-gateway-pos-fap/service-subtrair/{$novaRota}";
+
+            $valorA = $request->getParsedBody()["valorA"];
+            $valorB = $request->getParsedBody()["valorB"];
+
+            $config = curl_init($url);
+            curl_setopt($config, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($config, CURLOPT_POST, true);
+            curl_setopt($config, CURLOPT_POSTFIELDS, array(
+                "valorA" => $valorA,
+                "valorB" => $valorB
+            ));
+
+            $resposta = curl_exec($config);
+            curl_close($config);
+
+            return $response->withJson(
+                json_decode($resposta, true)
+            );
+        }
+    });
+});
+
 /* START */
 $app->run();
